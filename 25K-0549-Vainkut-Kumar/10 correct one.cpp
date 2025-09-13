57bd68ca928434b0965a58843f5abb9a11814eb1
Vainkut Kumar <vainkut.kumar@gmail.com>
2025-09-13 12:09:14 +0500

    Lab03 submission by 25K-0549 Vainkut Kumar

---
+++ 25K-0549-Vainkut-Kumar/10 correct one.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Using escape sequences to format output
+    printf("Full name : Vainkut Kumar\n");
+    printf("Fats nu ID : 25K-0549\n");
+    printf("\tTabbed line\n");
+    printf("\nNew line\n");
+
+    return 0;
+}
